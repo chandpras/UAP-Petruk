70d14a4bc630574ea604f28c6c9fc69a966031ec
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-06-14 21:24:13 +0700

    Update UAP Petruk.cpp

--- UAP Petruk.cpp
+++ UAP Petruk.cpp
@@ -4,17 +4,11 @@
 #define max 5
 using namespace std;
 
+queue<int> cart;
 string item_list[] = {"Mouse A1","Keyboard B2","Flashdisk C3","Monitor D4","Printer E5","Router F6","Harddisk G7","USB Port H8","Kabel I9"};
 int item_price[] = {120000,470000,95000,1600000,1150000,230000,560000,35000,22000};
 
-queue<int> cart;
-
 int main()
-{
-	
-}
-
-void checkout()
 {
 	
 }
