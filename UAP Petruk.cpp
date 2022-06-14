dd9c4573cadc3777bf3a95a7539b6e438f8599a3
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-06-14 21:39:03 +0700

    Update UAP Petruk.cpp

--- UAP Petruk.cpp
+++ UAP Petruk.cpp
@@ -10,5 +10,21 @@ int item_price[] = {25000,162000,45500,17500,19000,50000,28000,20000,10000};
 
 int main()
 {
-	
+	cout << "============================================\n";
+    	cout << "\t\tAPOTEK ANTI WARAS" << endl;
+    	cout << "============================================\n";
+    	cout << endl;
+    	cout << "List Barang : " << endl;
+    	for(int i = 0; i < 9; i++)
+    	{
+    		cout << i+1 << ". " << item_list[i] << "\t[Rp. " << item_price[i] << "]" << endl;
+		}
+		cout << "============================================\n";
+		cout << "\n1). Tambah Barang Ke Keranjang";
+		cout << "\n2). Hapus Barang Dari Keranjang";
+		cout << "\n3). Kosongkan Keranjang";
+		cout << "\n4). Cek Keranjang";
+		cout << "\n5). Keluar Program";
+		cout << "\n>> Masukkan Pilihan : ";
+		cin >> choose;
 }
