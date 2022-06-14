648b64db4aee714830216d03005b726568cae8b1
Chandra Prasetya Putra <93764458+chandpras@users.noreply.github.com>
2022-06-14 21:41:19 +0700

    Update UAP Petruk.cpp

--- UAP Petruk.cpp
+++ UAP Petruk.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 queue<int> cart;
-string item_list[] = {"Curcuma Plus Anak","Curfos Sirup","Vitacur Sirup","Kitavit Sirup","Hufagrip sirup","Imboost Force","Combantrin","Vitamin C","Promag"};
+string item_list[] = {"Curcuma Plus Sirup","Curfos Sirup","Vitacur Sirup","Kitavit Sirup","Hufagrip Sirup",
+		      "Imboost Force Tablet","Combantrin Sirup","Vitacimin tablet","Promag Tablet"};
 int item_price[] = {25000,162000,45500,17500,19000,50000,28000,20000,10000};
 
 int main()
